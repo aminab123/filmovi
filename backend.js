822477da07fe513adad076aa029bdc408eb40128
Amina Balavac <abalavac1@etf.unsa.ba>
2020-01-19 14:51:42 +0100

    Moj prvi commit

---
+++ backend.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var busboyBodyParser = require('busboy-body-parser');
+var app = express();
+
+app.use(busboyBodyParser());
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/', { useNewUrlParser: true });
+
+var Movie = mongoose.model('movie', new mongoose.Schema({
+    title: String,
+    img: String,
+    description: String
+}));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    obradiZahtjev(res);
+});
+
+function obradiZahtjev(res) {
+    Movie.find({}, function (err, movies) {
+        res.render('home', { movies: movies });
+    });
+}
+
+app.get('/movie', function(req,res) {
+    res.render('movie');
+});
+
+app.get('/addMovie', function (req,res) {
+    res.render('add-movie');
+});
+
+app.post('/addMovie', function (req, res) {
+    Movie.insertMany([{ ...req.body, img: Buffer.from(req.files.img.data).toString('base64') }], function () {
+        res.redirect('/');
+    });
+});
+
+app.get('/getAllMovis', function (req, res) {
+    //fetch all movies
+    
+});
+
+var port = 8080;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
